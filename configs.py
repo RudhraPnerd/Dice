889938030fe4fd263bfd06d5b53cd296af444de9
RudhraPnerd <rudhra.thiru@gmail.com>
2025-08-20 17:10:09 +1000

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
 PROGRAM_WORD = 'Program' # This program word constant is the word to use for program like instead you could use app.
 PROGRAM_NAME = f'Dice {PROGRAM_WORD}'
 GREETING_MESSAGE = f'Welcome to the {PROGRAM_NAME}'
-ROLLS_SAVE_FILE = '/Users/rudhra/PyCharmProjects/Dice/rolls.txt'
+ROLLS_SAVE_FILE = '/Users/rudhra/PyCharmProjects/Dice/data/rolls.txt'
