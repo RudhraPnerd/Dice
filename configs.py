4a92f6236ac096977e8b83b728c400615bfeb27b
RudhraPnerd <rudhra.thiru@gmail.com>
2025-08-20 17:09:12 +1000

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-PROGRAM = 'Dice Roller'
-GREETING_MESSAGE = f'Welcome to {PROGRAM}!'
-ROLLS_SAVE_FILE = 'data/rolls'
-DELAY = 0.01
+PROGRAM_WORD = 'Program' # This program word constant is the word to use for program like instead you could use app.
+PROGRAM_NAME = f'Dice {PROGRAM_WORD}'
+GREETING_MESSAGE = f'Welcome to the {PROGRAM_NAME}'
+ROLLS_SAVE_FILE = '/Users/rudhra/PyCharmProjects/Dice/rolls.txt'
